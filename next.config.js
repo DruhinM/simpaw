04c203022233fddc88b03c4a784b6b3d005d87aa
Druhin Mukherjee <druhin@Druhins-MacBook-Air.local>
2025-05-25 15:12:22 +0530

    fix: Update Next.js config for images and env variables

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    SHEET_ID: process.env.SHEET_ID,
-    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      }
     ],
     unoptimized: true,
+  },
+  env: {
+    SHEET_ID: process.env.SHEET_ID,
+    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
   },
   output: 'standalone',
 }
